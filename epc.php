bc19a8840f394639e4de6f27f5a8dbb6352817de
pkarnati2004 <pkarnati@mit.edu>
2020-01-24 15:36:43 -0500

    updating epc deadlines

--- epc.php
+++ epc.php
@@ -37,7 +37,7 @@
 
     <div class="row text-center">
         <a class="register-btn-large" style="text-decoration: none;" href="https://mit.co1.qualtrics.com/jfe/form/SV_4Ph3eoy0aLnOX5P?fbclid=IwAR3mZy3mnn6quniFePJ0UbTAQMW4coPGMqi1A0moyn_hR6xxYoX3sRUy_QQ">Apply Here</a>
-        <p>BY FRIDAY, 24th FEBRUARY 2020</p>
+        <p>BY FRIDAY, 14th FEBRUARY 2020 (Deadline Extended from 24th January)</p>
     </div>
 </div>
 
@@ -111,9 +111,9 @@
                 <p>The main deadlines:</p>
 
                 <ul class="a"> 
-                    <li>Friday, 24th January 2020: Application Deadline</li>
-                    <li>Friday, 7th February 2020: Individual Selections Announced</li>
-                    <li>Friday, 14th February 2020: Individual Confirmations Due</li>
+                    <li>Friday, 14th February 2020: Application Deadline (Deadline Extended from 24th January)</li>
+                    <li>Friday, 21st February 2020: Individual Selections Announced</li>
+                    <li>Friday, 28th February 2020: Individual Confirmations Due</li>
                     <li>February-March 2020: Mandatory 1st Mentorship Session with MIT Entrepreneurs</li>
                     <li>Monday, 23rd March 2020: Final Mentorship session at GSW</li>
                     <li>Full EPC deadlines <a href="assets/doc/epc.pdf">here.</a></li>
